512252108d351f92efda5c2311ae31c1f6d0f935
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2020-05-06 16:39:05 -0300

    Created Logo component

---
+++ components/Logo.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'Logo',
+  components: {
+    Influu: () => import('@/assets/img/logo-influu.svg?inline'),
+    MediaKit: () => import('@/assets/img/logo-midiakit.svg?inline')
+  }
+}
+</script>
+
+<template>
+  <nuxt-link to="/" class="leading-none text-white focusable hoverable hover:text-pink-800 outline-offset-outer">
+    <h1 class="inline-flex items-center ">
+      <Influu class="flex-grow mr-3" />
+      <MediaKit class="flex-grow" />
+    </h1>
+  </nuxt-link>
+</template>
