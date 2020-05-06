835dbf26b5e2db83c98b8faaf2f7f21d608bf86d
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2020-05-06 16:39:16 -0300

    Updated sample index layout

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,9 @@ export default {
 </script>
 
 <template>
-  <main id="index" class="p-4">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora eius labore laborum ut cumque neque, magnam dignissimos animi alias voluptatibus optio? Similique beatae laboriosam vero eum deserunt possimus velit reprehenderit.</p>
+  <main id="index">
+    <div class="container max-w-screen-xl">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora eius labore laborum ut cumque neque, magnam dignissimos animi alias voluptatibus optio? Similique beatae laboriosam vero eum deserunt possimus velit reprehenderit.</p>
+    </div>
   </main>
 </template>
