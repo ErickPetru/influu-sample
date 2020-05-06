9be78b88a90e5186a4bf72dcdf69febe3e8202ca
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2020-05-06 20:24:39 -0300

    Updated to use influencer data

--- components/Header.vue
+++ components/Header.vue
@@ -6,6 +6,11 @@ export default {
   directives: {
     resize
   },
+  filters: {
+    'thousands-separator' (value) {
+      return Number(value).toLocaleString()
+    }
+  },
   components: {
     Logo: () => import('@/components/Logo'),
     Menu: () => import('@/components/Menu'),
@@ -18,6 +23,12 @@ export default {
     IconTwitter: () => import('@/assets/img/icon-twitter.svg?inline'),
     IconSearch: () => import('@/assets/img/icon-search.svg?inline'),
     IconMenu: () => import('@/assets/img/icon-menu.svg?inline')
+  },
+  props: {
+    influencer: {
+      type: Object,
+      default: null
+    }
   },
   data () {
     return {
@@ -61,64 +72,66 @@ export default {
         <Logo />
 
         <nav class="overflow-hidden">
-          <a
-            href="#menu"
-            class="flex items-center px-2 py-3 hoverable focusable hover:text-pink-800 lg:hidden"
-            @click.prevent="showMenu = !showMenu"
-          >
-            <IconMenu class="h-5" />
-          </a>
-
           <transition name="fade">
             <Menu v-if="isMenuVisible" @close="showMenu = false" />
           </transition>
 
-          <ul class="hidden text-sm font-light uppercase lg:flex">
-            <li class="flex mr-1">
-              <a
-                href="#diversos"
-                class="px-4 py-2 hoverable focusable hover:text-pink-800"
-                @click.prevent
-              >Diversos</a>
-            </li>
-            <li class="flex mr-1">
-              <a
-                href="#influenciadores"
-                class="px-4 py-2 hoverable focusable hover:text-pink-800"
-                @click.prevent
-              >Influenciadores</a>
-            </li>
-            <li class="flex mr-1">
-              <a
-                href="#marketing"
-                class="px-4 py-2 hoverable focusable hover:text-pink-800"
-                @click.prevent
-              >Marketing</a>
-            </li>
-            <li class="flex mr-1">
-              <a
-                href="#noticias"
-                class="px-4 py-2 hoverable focusable hover:text-pink-800"
-                @click.prevent
-              >Notícias</a>
-            </li>
-            <li class="flex mr-1">
-              <a
-                href="#canal-influu"
-                class="px-4 py-2 hoverable focusable hover:text-pink-800"
-                @click.prevent
-              >Canal Influu</a>
-            </li>
-            <li class="flex">
-              <a
-                href="#pesquisar"
-                class="flex items-center px-2 py-1 hoverable focusable hover:text-pink-800"
-                @click.prevent
-              >
-                <IconSearch class="h-4" />
-              </a>
-            </li>
-          </ul>
+          <div class="flex">
+            <ul class="hidden text-sm font-light uppercase lg:flex">
+              <li class="flex mr-1">
+                <a
+                  href="#diversos"
+                  class="px-4 py-2 hoverable focusable hover:text-pink-800"
+                  @click.prevent
+                >Diversos</a>
+              </li>
+              <li class="flex mr-1">
+                <a
+                  href="#influenciadores"
+                  class="px-4 py-2 hoverable focusable hover:text-pink-800"
+                  @click.prevent
+                >Influenciadores</a>
+              </li>
+              <li class="flex mr-1">
+                <a
+                  href="#marketing"
+                  class="px-4 py-2 hoverable focusable hover:text-pink-800"
+                  @click.prevent
+                >Marketing</a>
+              </li>
+              <li class="flex mr-1">
+                <a
+                  href="#noticias"
+                  class="px-4 py-2 hoverable focusable hover:text-pink-800"
+                  @click.prevent
+                >Notícias</a>
+              </li>
+              <li class="flex mr-1">
+                <a
+                  href="#canal-influu"
+                  class="px-4 py-2 hoverable focusable hover:text-pink-800"
+                  @click.prevent
+                >Canal Influu</a>
+              </li>
+              <li class="flex">
+                <a
+                  href="#pesquisar"
+                  class="flex items-center px-2 py-1 hoverable focusable hover:text-pink-800"
+                  @click.prevent
+                >
+                  <IconSearch class="h-4" />
+                </a>
+              </li>
+            </ul>
+
+            <a
+              href="#menu"
+              class="flex items-center px-2 py-3 hoverable focusable hover:text-pink-800 lg:hidden"
+              @click.prevent="showMenu = !showMenu"
+            >
+              <IconMenu class="h-5 lg:h-4" />
+            </a>
+          </div>
         </nav>
       </div>
     </div>
@@ -126,14 +139,16 @@ export default {
     <div class="container max-w-screen-xl lg:px-4">
       <div class="pt-4 lg:flex lg:pt-8 xl:pt-12">
         <img
-          src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjAwMzc5OTEzOF5BMl5BanBnXkFtZTgwMDc5ODU3MTE@._V1_UX172_CR0,0,172,256_AL_.jpg"
+          v-if="influencer && influencer.imageUrl"
+          :src="influencer.imageUrl"
           alt="Foto de perfil"
           class="z-20 object-cover w-40 h-40 max-w-sm m-auto mt-0 border-4 border-pink-200 rounded-full shadow-lg lg:flex-grow-0 lg:w-64 lg:h-64 xl:w-76 xl:h-76 lg:-mb-32"
         />
 
         <h2
+          v-if="influencer && influencer.name"
           class="mt-2 text-2xl font-light text-center lg:text-left lg:flex-grow lg:text-5xl lg:ml-10 lg:pt-6 xl:ml-12 xl:pt-10"
-        >Fulano de Tal</h2>
+        >{{ influencer.name }}</h2>
       </div>
     </div>
 
@@ -153,75 +168,77 @@ export default {
         />
       </a>
 
-      <transition name="slide-y">
+      <transition v-if="influencer" :name="isSmartphone ? 'slide-y' : null">
         <div v-show="isNetworksVisible" class="container max-w-screen-xl px-0 lg:px-4">
           <ul
             class="absolute justify-center w-full pt-3 mt-12 text-sm tracking-wide bg-purple-600 -top-6px md:relative md:pt-0 md:mt-0 md:top-0 md:flex lg:justify-start lg:pl-68 xl:pl-80"
           >
-            <li>
+            <li v-if="influencer.youtube">
               <a
-                href="https://www.youtube.com/"
+                :href="`https://www.youtube.com/${influencer.youtube.username}`"
                 target="_blank"
-                class="flex px-5 py-2 border-b hoverable focusable hover:text-pink-800 border-white-10 md:border-b-0 md:border-r md:border-white-40 md:py-4 lg:px-6 xl:px-8"
+                class="flex px-5 py-2 border-b hoverable focusable hover:text-pink-800 border-white-10 md:border-b-0 md:border-r md:border-white-40 md:py-4 lg:px-4 xl:px-8"
               >
                 <IconYouTube class="w-8 mr-3 text-2xl" />
 
                 <div class="leading-tight">
                   <p>
-                    <strong class="font-medium">/fulanodetal</strong>
+                    <strong class="font-medium">/{{ influencer.youtube.username }}</strong>
                   </p>
-                  <p class="font-normal">+ 3.520.000</p>
+                  <p class="font-normal">+ {{ influencer.youtube.followers | thousands-separator }}</p>
                 </div>
               </a>
             </li>
 
-            <li>
+            <li v-if="influencer.instagram">
               <a
-                href="https://www.instagram.com/"
+                :href="`https://www.instagram.com/${influencer.instagram.username}`"
                 target="_blank"
-                class="flex px-5 py-2 border-b hoverable focusable hover:text-pink-800 border-white-10 md:border-b-0 md:border-r md:border-white-40 md:py-4 lg:px-6 xl:px-8"
+                class="flex px-5 py-2 border-b hoverable focusable hover:text-pink-800 border-white-10 md:border-b-0 md:border-r md:border-white-40 md:py-4 lg:px-4 xl:px-8"
               >
-                <IconInstagram class="w-6 ml-1 mr-4 text-2xl" />
+                <IconInstagram class="w-6 ml-1 mr-2 text-2xl" />
 
                 <div class="leading-tight">
                   <p>
-                    <strong class="font-medium">@fulano_tal</strong>
+                    <strong class="font-medium">@{{ influencer.instagram.username }}</strong>
                   </p>
-                  <p class="font-normal">+ 1.020.000</p>
+                  <p
+                    class="font-normal"
+                  >+ {{ influencer.instagram.followers | thousands-separator }}</p>
                 </div>
               </a>
             </li>
 
-            <li>
+            <li v-if="influencer.facebook">
               <a
-                href="https://www.facebook.com/"
+                :href="`https://www.facebook.com/${influencer.facebook.username}`"
                 target="_blank"
-                class="flex px-5 py-2 border-b hoverable focusable hover:text-pink-800 border-white-10 md:border-b-0 md:border-r md:border-white-40 md:py-4 lg:px-6 xl:px-8"
+                class="flex px-5 py-2 border-b hoverable focusable hover:text-pink-800 border-white-10 md:border-b-0 md:border-r md:border-white-40 md:py-4 lg:px-4 xl:px-8"
               >
-                <IconFacebook class="w-6 ml-1 mr-4 text-2xl" />
+                <IconFacebook class="w-6 ml-1 mr-2 text-2xl" />
 
                 <div class="leading-tight">
                   <p>
-                    <strong class="font-medium">/fulanotal</strong>
+                    <strong class="font-medium">/{{ influencer.facebook.username }}</strong>
                   </p>
-                  <p class="font-normal">+ 21.900.000</p>
+                  <p class="font-normal">+ {{ influencer.facebook.followers | thousands-separator }}</p>
                 </div>
               </a>
             </li>
 
-            <li>
+            <li v-if="influencer.twitter">
               <a
-                href="https://www.twitter.com/"
+                :href="`https://www.twitter.com/${influencer.twitter.username}`"
                 target="_blank"
-                class="flex px-5 py-2 hoverable focusable hover:text-pink-800 md:py-4 lg:px-6 xl:px-8"
+                class="flex px-5 py-2 hoverable focusable hover:text-pink-800 md:py-4 lg:px-4 xl:px-8"
               >
-                <IconTwitter class="w-6 ml-1 mr-4 text-2xl" />
+                <IconTwitter class="w-6 ml-1 mr-2 text-2xl" />
 
                 <div class="leading-tight">
                   <p>
-                    <strong class="font-medium">@fulanotal</strong>
+                    <strong class="font-medium">@{{ influencer.twitter.username }}</strong>
                   </p>
-                  <p class="font-normal">+ 18.300.000</p>
+                  <p class="font-normal">+ {{ influencer.twitter.followers | thousands-separator }}</p>
                 </div>
               </a>
             </li>
